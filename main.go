5c5c01ad36ebf1e37a75f37603f1bf87d7cf0906
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2026-09-14 21:28:21 +0200

    feat: use new jamd

--- main.go
+++ main.go
@@ -38,11 +38,13 @@ func run(args []string) error {
 	hostPort := fmt.Sprintf("%s:%d", cfg.host, cfg.port)
 	slog.Info("listen and serve", "scheme", cfg.scheme, "host", cfg.host, "port", cfg.port)
 
+	j := NewJamd(cfg)
+
 	switch strings.ToLower(cfg.scheme) {
 	case "http":
-		err = http.ListenAndServe(hostPort, nil)
+		err = http.ListenAndServe(hostPort, j)
 	case "https":
-		err = http.ListenAndServeTLS(hostPort, cfg.certFile, cfg.keyFile, nil)
+		err = http.ListenAndServeTLS(hostPort, cfg.certFile, cfg.keyFile, j)
 	default:
 		return fmt.Errorf("%w: %q", ErrConfigWrongScheme, cfg.scheme)
 	}
