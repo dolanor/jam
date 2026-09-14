9f4e695190a62df53374500aafb8cb564b818073
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2026-09-14 21:28:48 +0200

    feat: add naive proxy mapping

--- config.go
+++ config.go
@@ -11,9 +11,11 @@ import (
 )
 
 var (
-	ErrConfig                      = errors.New("config")
-	ErrConfigWrongScheme           = fmt.Errorf("%w: %w", ErrConfig, errors.New("wrong scheme"))
-	ErrConfigTLSCertKeyFileMissing = fmt.Errorf("%w: %w", ErrConfig, errors.New("cert or key file not present for https"))
+	ErrConfig                                 = errors.New("config")
+	ErrConfigWrongScheme                      = fmt.Errorf("%w: %w", ErrConfig, errors.New("wrong scheme"))
+	ErrConfigTLSCertKeyFileMissing            = fmt.Errorf("%w: %w", ErrConfig, errors.New("cert or key file not present for https"))
+	ErrConfigHost2BackendMapping              = fmt.Errorf("%w: %w", ErrConfig, errors.New("error in host to backend mapping"))
+	ErrConfigHost2BackendMappingDuplicateHost = fmt.Errorf("%w: %w", ErrConfigHost2BackendMapping, errors.New("duplicate host entry"))
 )
 
 type config struct {
@@ -23,10 +25,15 @@ type config struct {
 
 	certFile string
 	keyFile  string
+
+	host2backend map[string]string
 }
 
 func loadConfig(args []string) (config, error) {
-	cfg := config{}
+	cfg := config{
+		host2backend: map[string]string{},
+	}
+	var host2backend []string
 
 	fs := ff.NewFlagSet("jamd")
 	fs.StringVar(&cfg.scheme, 'S', "scheme", "http", "scheme to listen on")
@@ -34,6 +41,7 @@ func loadConfig(args []string) (config, error) {
 	fs.IntVar(&cfg.port, 'P', "port", 44444, "port to listen on")
 	fs.StringVar(&cfg.certFile, 0, "cert-file", "", "cert file path for the TLS configuration")
 	fs.StringVar(&cfg.keyFile, 0, "key-file", "", "key file path for the TLS configuration")
+	fs.StringListVar(&host2backend, 0, "host2backend", "mapping between host to its backend")
 
 	err := ff.Parse(fs, args,
 		ff.WithEnvVarPrefix("JAMD"),
@@ -49,6 +57,20 @@ func loadConfig(args []string) (config, error) {
 		if cfg.certFile == "" || cfg.keyFile == "" {
 			return cfg, ErrConfigTLSCertKeyFileMissing
 		}
+	}
+
+	for _, v := range host2backend {
+		host, backend, ok := strings.Cut(v, ":")
+		if !ok {
+			return cfg, ErrConfigHost2BackendMapping
+		}
+
+		_, ok = cfg.host2backend[host]
+		if ok {
+			return cfg, ErrConfigHost2BackendMappingDuplicateHost
+		}
+
+		cfg.host2backend[host] = backend
 	}
 
 	return cfg, nil
